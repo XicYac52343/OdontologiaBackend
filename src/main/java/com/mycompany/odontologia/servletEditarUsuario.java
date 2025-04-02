1bf826c32a79d410ab135636bd777d6e3e70253e
JefersonDavidVegaRom <JefersonDavidVegaRom@P-1401-009252>
2025-04-01 19:41:29 -0500

    Se añade Editar Usuarios

--- src/main/java/com/mycompany/odontologia/servletEditarUsuario.java
+++ src/main/java/com/mycompany/odontologia/servletEditarUsuario.java
@@ -2,17 +2,20 @@ package com.mycompany.odontologia;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import javax.servlet.http.HttpSession;
+import logica.claseUsuariosOdon;
+import logica.controladora;
 
 @WebServlet(name = "servletEditarUsuario", urlPatterns = {"/servletEditarUsuario"})
 public class servletEditarUsuario extends HttpServlet {
-
-
+    controladora control = new controladora();
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -21,7 +24,7 @@ public class servletEditarUsuario extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet servletEditarUsuario</title>");            
+            out.println("<title>Servlet servletEditarUsuario</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet servletEditarUsuario at " + request.getContextPath() + "</h1>");
@@ -30,18 +33,42 @@ public class servletEditarUsuario extends HttpServlet {
         }
     }
 
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        List<claseUsuariosOdon> listaUsuarios = new ArrayList<>();
+        listaUsuarios = control.traerUsuario();
+        HttpSession misession = request.getSession();
+        int id = Integer.parseInt(request.getParameter("id"));
+        
+        for(int i = 0; i < listaUsuarios.size(); i++){
+            if(listaUsuarios.get(i).getId() == id){
+               misession.setAttribute("usuarioEditar", listaUsuarios.get(i));
+               response.sendRedirect("verUsuarios.jsp");
+            }
+        }
     }
-
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        String nombre = request.getParameter("nombre");
+        String apellido = request.getParameter("apellido");
+        String correo = request.getParameter("correo");
+        String contrasenia = request.getParameter("contrasenia");
+        String rol = request.getParameter("rol");
+        
+        claseUsuariosOdon usu = new claseUsuariosOdon();
+        
+        usu.setNombre(nombre);
+        usu.setApellido(apellido);
+        usu.setCorreo(correo);
+        usu.setContrasenia(contrasenia);
+        usu.setRol(rol);
+        
+        control.editarUsuario(usu);
+        response.sendRedirect("verUsuarios.jsp");
+
     }
 
     @Override
